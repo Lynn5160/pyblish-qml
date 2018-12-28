f5c6c93b6f24282901b5e1699dab7f4934fdcbc4
David Lai <david962041@gmail.com>
2018-12-28 19:09:03 +0800

    Add comment

--- pyblish_qml/control.py
+++ pyblish_qml/control.py
@@ -517,6 +517,7 @@ class Controller(QtCore.QObject):
     def toggleSection(self, checkState, sectionLabel):
         model = self.data["models"]["item"]
 
+        # Get all items' current toggle state
         states = set()
         for item in model.items:
             if item.itemType == "instance" and sectionLabel == item.category:
@@ -528,6 +529,8 @@ class Controller(QtCore.QObject):
                     states.add(item.isToggled)
 
         if len(states) == 1:
+            # Use items' states instead of section state if all optional items
+            # are the same state.
             checkState = not states.pop()
 
         for item in model.items:
